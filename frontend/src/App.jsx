bba5cc24f56b8e8a935e8a501ea5122d50ef2680
flashgrey <aaftabkhan2005@gmail.com>
2026-04-17 14:52:33 +0530

    Beautiful frontend update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,121 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Button from './ui/Button';
+import './App.css';
+import cloud3 from './assets/cloud3.svg';
+import Navbar from './ui/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const handleFishing = () => {
+    fetch('http://localhost:3000/api/message')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <div className="sky">
+        <div className="cloud-layer bg"></div>
 
-      <div className="ticks"></div>
+        <img src={cloud3} className="cloud c1" />
+        <img src={cloud3} className="cloud c2" />
+        <img src={cloud3} className="cloud c3" />
+      </div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      {/* <Navbar /> */}
+      <br />
+      <br /><br /><br /><br /><br /><br /><br />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+      <h1 style={{fontFamily: 'Winky Rough',
+        fontWeight: 300,
+        fontStyle: 'italic'
+      }}>Umi ni bin</h1>
+      <div style={{justifyContent: 'space-ariund',
+        width: '325px',
+        display: 'flex'
+      }}>
+        <Button value="Write a letter" />
+        <Button value={"Try fishing"} onClick={handleFishing} />
+      </div>
+
+
+      <div className='sea'>
+        <div className='wave wave1'></div>
+        <div className='wave wave2'></div>
+        <div className='wave wave3'></div>
+        <div className='wave wave4'></div>
+      </div>
+    </div>
+  );
 }
 
 export default App
---
+++ frontend/src/ui/Button.jsx
@@ -0,0 +1,10 @@
+import styles from './Button.module.css'
+
+function Button({ value, onClick }) {
+
+    return (
+        <button className={styles.button} onClick={onClick} >{value}</button>
+    );
+}
+
+export default Button
